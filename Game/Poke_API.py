02c897b96448a67ddf790a45db92e28b5a652edc
Ebaad Waseem <ebaadwaseem@gmail.com>
2023-05-10 09:07:32 -0600

    Added Poke_API in case of future need

---
+++ Game/Poke_API.py
@@ -0,0 +1,34 @@
+import requests, random
+
+# Initialize an empty list to store the data
+pokemon_data = []
+
+# Repeat the API call 12 times
+for i in range(12):
+    # Generate a random Pokemon number
+    pokemon = random.randint(1, 151)
+
+    # Construct the URL for the PokeAPI request
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}/'
+
+    # Send a GET request to the PokeAPI URL
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # the request was successful
+        data = response.json()
+        # Append the data to the list
+        pokemon_data.append(data)
+    else:
+        # handle the error
+        print(f"Error: {response.status_code}")
+
+# Print the data for all 12 Pokemon
+for data in pokemon_data:
+    print(f"Name: {data['name']}")
+    print(f"ID: {data['id']}")
+    print(f"Height: {data['height']}")
+    print(f"Weight: {data['weight']}")
+    #print("Abilities:")
+    #for ability in data['abilities']:
+        #print(f" - {ability['ability']['name']}")
