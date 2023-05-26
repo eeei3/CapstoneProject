df02f8628d4329dfe74ed2c006e7118fa840149d
shitass <joshua0307lol@gmail.com>
2023-05-26 08:37:06 -0600

    updated battle.py

--- Game/battle.py
+++ Game/battle.py
@@ -11,28 +11,31 @@ Current Assignment: battle.py
 """
 # Important package imports
 import enemies
+from tkinter import *
 
 
 class LBattle:
     def __init__(self):
         self.lvl = 1
-        self.p1 = None
-        self.p2 = enemies.Trainer(self.lvl)
-        return
+        """self.p1 = None
+        self.p2 = enemies.Trainer(self.lvl)"""
+        self.root = Tk()
 
-    def start_battle(self):
+    """def start_battle(self):
         self.p1.turn(self.p2.played_pokemon)
-        self.p2.turn(self.p1.played_pokemon)
+        self.p2.turn(self.p1.played_pokemon)"""
 
     def gamewin(self):
-        self.root.deiconify()
-        self.main.withdraw()
         self.root.geometry("900x500")
         self.root.title("Pokémon Battle")
-        self.fight.pack(side=RIGHT)
-        self.team.pack(side=LEFT)
-        self.bag.pack(side=BOTTOM)
-        self.run.pack(side=BOTTOM)
+        fight = Button(self.root, text="fight")
+        team = Button(self.root, text="team")
+        bag = Button(self.root, text="bag")
+        run = Button(self.root, text="run")
+        fight.pack(side=RIGHT)
+        team.pack(side=LEFT)
+        bag.pack(side=BOTTOM)
+        run.pack(side=BOTTOM)
         self.root.mainloop()
 
 
@@ -43,3 +46,6 @@ class NBattle(LBattle):
         self.p2 = None
         self.stream = stream
         return
+
+a = LBattle()
+a.gamewin()
